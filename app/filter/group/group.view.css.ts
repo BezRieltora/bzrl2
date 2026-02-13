73541af48ed0b29dbd78bb6bbcf6221473a20543
novgorodcev.k <novgorodcev.k@rwb.ru>
2026-02-14 02:21:23 +0300

    ыва
    Signed-off-by: novgorodcev.k <novgorodcev.k@rwb.ru>

--- app/filter/group/group.view.css.ts
+++ app/filter/group/group.view.css.ts
@@ -2,13 +2,17 @@ namespace $.$$ {
 	$mol_style_define($bog_bzrl_app_filter_group, {
 		border: {
 			radius: $mol_gap.round,
-			style: 'solid',
 			color: '#E0E0E0',
 			width: '1px',
 		},
 		display: 'flex',
+		background: {
+			color: '#F5F5F5',
+		},
+
 		align: {
 			items: 'center',
 		},
+		color: $mol_theme.text,
 	})
 }
