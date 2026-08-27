3af10b17061929faec03e0edcaef5e175f7f1bb8
Dev-cmyser <Dev-cmyser@users.noreply.github.com>
2026-08-27 18:12:10 +0000

    Deploying to gh-pages from @ BezRieltora/bzrl2@8ca753da8d9f2f2286c7cc0a7df4060294c70af1 🚀

--- web.js
+++ web.js
@@ -28289,7 +28289,7 @@ var $;
     (function ($$) {
         $mol_style_define($giper_baza_app_stat_page, {
             flex: {
-                basis: `40rem`,
+                basis: `100%`,
                 grow: 1,
             },
             Main: {
@@ -28426,7 +28426,7 @@ var $;
         $mol_style_define($giper_baza_app_stat_list, {
             Menu: {
                 flex: {
-                    basis: '25rem',
+                    basis: '30rem',
                 },
             },
             Menu_link: {
