dc7f13b9f9f0534a037c59e7f49359c5f4fbebd6
Dev-cmyser <Dev-cmyser@users.noreply.github.com>
2026-09-19 11:53:15 +0000

    Deploying to gh-pages from @ BezRieltora/bzrl2@8ca753da8d9f2f2286c7cc0a7df4060294c70af1 🚀

--- node.test.js
+++ node.test.js
@@ -6365,6 +6365,7 @@ var $;
         ol: {},
         li: {},
         details: {},
+        section: {},
         summary: {},
         hr: {},
         table: {},
@@ -13111,13 +13112,11 @@ var $;
                 clearInterval(interval);
                 setTimeout(() => this.reconnects(null), 1000);
             };
-            Object.assign(socket, {
-                destructor: () => {
-                    socket.onclose = () => { };
-                    clearInterval(interval);
-                    socket.close();
-                }
-            });
+            port.destructor = () => {
+                socket.onclose = () => { };
+                clearInterval(interval);
+                socket.close();
+            };
             return new Promise((done, fail) => {
                 socket.onopen = () => {
                     this.$.$mol_log3_come({
@@ -16061,14 +16060,6 @@ var $;
 "use strict";
 var $;
 (function ($) {
-    /** @deprecated Use $mol_crypto2_hash */
-    $.$mol_crypto_hash = $mol_crypto2_hash;
-})($ || ($ = {}));
-
-;
-"use strict";
-var $;
-(function ($) {
     class $mol_rest_server extends $mol_object {
         log() {
             return this.$.$mol_state_arg.value('mol_rest_server_log') !== null;
@@ -16181,13 +16172,14 @@ var $;
                     });
                     return;
                 }
+                socket.end();
             });
             socket.on('end', onclose);
             socket.on('error', onclose);
             socket.on('data', (chunk) => this.ws_income(chunk, upgrade, socket));
             const key_in = req.headers["sec-websocket-key"];
             const magic = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11';
-            const key_out = $mol_base64_encode($mol_crypto_hash($mol_charset_encode(key_in + magic)));
+            const key_out = $mol_base64_encode($mol_crypto2_hash($mol_charset_encode(key_in + magic)));
             socket.write('HTTP/1.1 101 WS Handshaked\r\n' +
                 'Upgrade: WebSocket\r\n' +
                 'Connection: Upgrade\r\n' +
